ba5cc35a7001f1ee5421a07cf9d90f6244edf99b
ThePope05 <swobben05@gmail.com>
2024-06-25 14:19:49 +0200

    Seeder toegevoegd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Allergy;
+use App\Models\Klant;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,81 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Klant::factory(10)->create();
+
+        Allergy::insert([
+            [
+                'naam' => 'Gluten',
+                'beschrijving' => 'Gluten is een eiwit dat voorkomt in tarwe, rogge en gerst.'
+            ],
+            [
+                'naam' => 'Lactose',
+                'beschrijving' => 'Lactose is een suiker die van nature voorkomt in melk en melkproducten.',
+            ],
+            [
+                'naam' => 'Pinda',
+                'beschrijving' => 'Pinda is een peulvrucht die in allerlei producten kan zitten.',
+            ],
+            [
+                'naam' => 'Schaaldieren',
+                'beschrijving' => 'Schaaldieren zijn waterdieren met een uitwendig skelet, zoals garnalen, krab, kreeft en rivierkreeft.'
+            ],
+            [
+                'naam' => 'Soja',
+                'beschrijving' => 'Soja is een peulvrucht die in allerlei producten kan zitten.'
+            ],
+            [
+                'name' => 'Noten',
+                'beschrijving' => 'Noten zijn vruchten met een harde schaal die aan bomen groeien.'
+            ]
+
+        ]);
+
+        DB::table('klanten_allergenen')->insert([
+            [
+                'klant_id' => 1,
+                'allergie_id' => 1,
+            ],
+            [
+                'klant_id' => 1,
+                'allergie_id' => 2,
+            ],
+            [
+                'klant_id' => 2,
+                'allergie_id' => 3,
+            ],
+            [
+                'klant_id' => 3,
+                'allergie_id' => 4,
+            ],
+            [
+                'klant_id' => 4,
+                'allergie_id' => 5,
+            ],
+            [
+                'klant_id' => 5,
+                'allergie_id' => 6,
+            ],
+            [
+                'klant_id' => 6,
+                'allergie_id' => 1,
+            ],
+            [
+                'klant_id' => 7,
+                'allergie_id' => 2,
+            ],
+            [
+                'klant_id' => 8,
+                'allergie_id' => 3,
+            ],
+            [
+                'klant_id' => 9,
+                'allergie_id' => 4,
+            ],
+            [
+                'klant_id' => 10,
+                'allergie_id' => 5,
+            ]
         ]);
     }
 }
