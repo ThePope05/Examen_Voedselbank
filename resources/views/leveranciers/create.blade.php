02f2a23dfe4a55c242c4c5d3de6f028885856bf8
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-06-25 11:11:49 +0200

    Commentaar toegevoegd

--- resources/views/leveranciers/create.blade.php
+++ resources/views/leveranciers/create.blade.php
@@ -5,7 +5,9 @@
     <h1>Nieuwe Leverancier</h1>
     <form action="{{ route('leveranciers.store') }}" method="POST">
         @csrf
-        @include('leveranciers.form')
+        @include('leveranciers.form') <!-- Inclusie van het formulier voor leveranciers -->
+
+        <!-- Knop om het formulier op te slaan -->
         <button type="submit" class="btn btn-primary">Opslaan</button>
     </form>
 </div>
