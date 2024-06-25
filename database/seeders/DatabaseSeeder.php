9f901022a3dcd7fcf8fb5892b06899c4718d796d
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2024-06-25 13:40:47 +0200

    Voorraad branch gemerded naar dev branch alle merge conflicts gefixed en allebie de features werkent

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,13 @@
 <?php
+
 use Illuminate\Database\Seeder;
-use App\Models\Leverancier;
+use Database\Seeders\CategorieTableSeeder;
+use Database\Seeders\LeveranciersTableSeeder;
+use Database\Seeders\KlantenTableSeeder;
+use Database\Seeders\ProductenTableSeeder;
+use Database\Seeders\VoedselpakkettenTableSeeder;
+use Database\Seeders\VoedselpakketProductenTableSeeder;
+use Database\Seeders\ProductAllergyTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,50 +21,6 @@ class DatabaseSeeder extends Seeder
             VoedselpakkettenTableSeeder::class,
             VoedselpakketProductenTableSeeder::class,
             ProductAllergyTableSeeder::class,
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // 5 Leveranciers aanmaken
-        Leverancier::create([
-            'bedrijfsnaam' => 'Bedrijf A',
-            'adres' => 'Straatweg 1, 1234 AB Stad',
-            'contactpersoon' => 'Jan de Vries',
-            'email' => 'jan@bedrijfa.nl',
-            'telefoonnummer' => '012-3456789',
-        ]);
-
-        Leverancier::create([
-            'bedrijfsnaam' => 'Bedrijf B',
-            'adres' => 'Dorpsstraat 2, 2345 CD Dorp',
-            'contactpersoon' => 'Erik Bakker',
-            'email' => 'erik@bedrijfb.nl',
-            'telefoonnummer' => '023-4567890',
-        ]);
-
-        Leverancier::create([
-            'bedrijfsnaam' => 'Bedrijf C',
-            'adres' => 'Laan 3, 3456 DE Gehucht',
-            'contactpersoon' => 'Sara Groen',
-            'email' => 'sara@bedrijfc.nl',
-            'telefoonnummer' => '034-5678901',
-        ]);
-
-        Leverancier::create([
-            'bedrijfsnaam' => 'Bedrijf D',
-            'adres' => 'Weg 4, 4567 EF Plek',
-            'contactpersoon' => 'Pieter Zwart',
-            'email' => 'pieter@bedrijfd.nl',
-            'telefoonnummer' => '045-6789012',
-        ]);
-
-        Leverancier::create([
-            'bedrijfsnaam' => 'Bedrijf E',
-            'adres' => 'Pad 5, 5678 GH Oord',
-            'contactpersoon' => 'Linda Wit',
-            'email' => 'linda@bedrijfe.nl',
-            'telefoonnummer' => '056-7890123',
         ]);
     }
 }
--- database/seeders/LeveranciersTableSeeder.php
+++ database/seeders/LeveranciersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use App\Models\Leverancier;
 
 class LeveranciersTableSeeder extends Seeder
 {
@@ -56,6 +57,46 @@ class LeveranciersTableSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ],
+        ]);
+        // 5 Leveranciers aanmaken
+        Leverancier::create([
+            'bedrijfsnaam' => 'Bedrijf A',
+            'adres' => 'Straatweg 1, 1234 AB Stad',
+            'contactpersoon' => 'Jan de Vries',
+            'email' => 'jan@bedrijfa.nl',
+            'telefoonnummer' => '012-3456789',
+        ]);
+
+        Leverancier::create([
+            'bedrijfsnaam' => 'Bedrijf B',
+            'adres' => 'Dorpsstraat 2, 2345 CD Dorp',
+            'contactpersoon' => 'Erik Bakker',
+            'email' => 'erik@bedrijfb.nl',
+            'telefoonnummer' => '023-4567890',
+        ]);
+
+        Leverancier::create([
+            'bedrijfsnaam' => 'Bedrijf C',
+            'adres' => 'Laan 3, 3456 DE Gehucht',
+            'contactpersoon' => 'Sara Groen',
+            'email' => 'sara@bedrijfc.nl',
+            'telefoonnummer' => '034-5678901',
+        ]);
+
+        Leverancier::create([
+            'bedrijfsnaam' => 'Bedrijf D',
+            'adres' => 'Weg 4, 4567 EF Plek',
+            'contactpersoon' => 'Pieter Zwart',
+            'email' => 'pieter@bedrijfd.nl',
+            'telefoonnummer' => '045-6789012',
+        ]);
+
+        Leverancier::create([
+            'bedrijfsnaam' => 'Bedrijf E',
+            'adres' => 'Pad 5, 5678 GH Oord',
+            'contactpersoon' => 'Linda Wit',
+            'email' => 'linda@bedrijfe.nl',
+            'telefoonnummer' => '056-7890123',
         ]);
     }
 }
