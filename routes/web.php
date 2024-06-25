7968177bc0a3e349b8d82a04cbacfaaf5f1dae2f
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-06-25 12:02:35 +0200

    route gemaakt

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoedselpakketController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//alle routes voor voedselpakketten
+Route::get('/voedselpakket', [VoedselpakketController::class, 'index'])->name('voedselpakket.index');
+Route::get('/voedselpakket/create', [VoedselpakketController::class, 'create'])->name('voedselpakket.create');
+Route::post('/voedselpakket', [VoedselpakketController::class, 'store'])->name('voedselpakket.store');
+Route::get('/voedselpakket/{voedselpakketten}/edit', [VoedselpakketController::class, 'edit'])->name('voedselpakket.edit');
+Route::put('/voedselpakket/{voedselpakketten}', [VoedselpakketController::class, 'update'])->name('voedselpakket.update');
+Route::delete('/voedselpakket/{voedselpakketten}', [VoedselpakketController::class, 'destroy'])->name('voedselpakket.destroy');
