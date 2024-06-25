d29d1d0df6c3cca02679d6b424994eddb74d4bfb
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-06-25 11:02:38 +0200

    routes voor leverancier gemaakt

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LeverancierController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//alle routes voor leveranciers
+Route::get('/leveranciers', [LeverancierController::class, 'index'])->name('leveranciers.index');
+Route::get('/leveranciers/create', [LeverancierController::class, 'create'])->name('leveranciers.create');
+Route::post('/leveranciers', [LeverancierController::class, 'store'])->name('leveranciers.store');
+Route::get('/leveranciers/{leverancier}', [LeverancierController::class, 'show'])->name('leveranciers.show');
+Route::get('/leveranciers/{leverancier}/edit', [LeverancierController::class, 'edit'])->name('leveranciers.edit');
+Route::put('/leveranciers/{leverancier}', [LeverancierController::class, 'update'])->name('leveranciers.update');
+Route::delete('/leveranciers/{leverancier}', [LeverancierController::class, 'destroy'])->name('leveranciers.destroy');
